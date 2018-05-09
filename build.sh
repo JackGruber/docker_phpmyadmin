7243e79e8fb517c08614228b8b6eeaeaa6fca7de
JackGruber <24863925+JackGruber@users.noreply.github.com>
2018-05-10 00:24:48 +0200

    add parameters

--- build.sh
+++ build.sh
@@ -1,30 +1,41 @@
 #!/bin/sh
-set -e
-cd $(dirname $0)
-
 REPO="jackgruber/phpmyadmin"
 
 arch=$( uname -m )
 
+if [ "$1" = "test" ]; then
+  arch="test"
+fi
+
 docker build -t $REPO:$arch .
 
-case $arch in
-armv7l)
-  docker tag $REPO:$arch $REPO:rpi
-  docker tag $REPO:$arch $REPO:rpi3
-  docker tag $REPO:$arch $REPO:arm
-  ;;
-armv6l)
-  docker tag $REPO:$arch $REPO:rpi
-  docker tag $REPO:$arch $REPO:rpizw
-  docker tag $REPO:$arch $REPO:arm
-  ;;
-x86_64)
-  ;;
-*)
-  echo "Unknown arch $( uname -m )"
-  exit 1
-  ;;
-esac
+if [ "$1" != "test" ]; then
+  case $arch in
+  armv7l)
+    docker tag $REPO:$arch $REPO:rpi
+    docker tag $REPO:$arch $REPO:rpi3
+    docker tag $REPO:$arch $REPO:arm
 
-docker push $REPO
+    # for history
+    docker tag $REPO:$arch $REPO'_rpi'
+    if [ "$1" = "push" ]; then
+      docker push $REPO'_rpi'
+    fi
+    ;;
+  armv6l)
+    docker tag $REPO:$arch $REPO:rpi
+    docker tag $REPO:$arch $REPO:rpizw
+    docker tag $REPO:$arch $REPO:arm
+    ;;
+  x86_64)
+    ;;
+  *)
+    echo "Unknown arch $( uname -m )"
+    exit 1
+  ;;
+  esac
+
+  if [ "$1" = "push" ]; then
+    docker push $REPO
+  fi
+fi
